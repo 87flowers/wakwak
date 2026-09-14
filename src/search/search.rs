071832490523b69dda6e3a4f43bf7ef8d32c00d4
Tecci <tecci.fi@gmail.com>
2026-09-14 22:37:14 +0300

    new bench
    Bench: 586345

--- src/search/search.rs
+++ src/search/search.rs
@@ -215,7 +215,7 @@ fn search<Node: NodeType>(
     }
 
     // TODO: uncomment this when it is used
-    //let in_check = pos.board().in_check();
+    // let in_check = pos.board().in_check();
     let raw_eval = eval(pos.board());
     let corr = thread.history.corr(pos.board());
     let static_eval = adjust_eval(raw_eval, corr);
