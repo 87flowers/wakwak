2f01694a460ba8a1ffd37869fea56a7cb95699e0
87 <dev@87flowers.com>
2026-09-16 12:21:18 +0200

    Bench: 1174174

--- src/position.rs
+++ src/position.rs
@@ -44,7 +44,9 @@ impl Position {
     #[inline]
     pub fn make_null_move(&mut self) {
         let duck = self.previous_boards.last().and_then(|b| b.duck());
+
         self.previous_boards.push(self.current);
+        self.previous_moves.push(None);
         self.current.make_null_move(duck);
     }
 
